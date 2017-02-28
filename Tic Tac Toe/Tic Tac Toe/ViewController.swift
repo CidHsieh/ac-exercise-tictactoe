6dcc3c60813bcfb4e818406144383060c614bb83
Cid Hsieh <cidhsieh@CidHsieh.local>
2017-02-28 21:45:19 +0800

    設定game state及player順序

--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -11,18 +11,25 @@ import UIKit
 class ViewController: UIViewController{
     
     var activePlayer = 1 //croee
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     @IBAction func action(_ sender: AnyObject)
     {
-        if activePlayer == 1
+        if gameState[sender.tag - 1] == 0
         {
-            sender.setImage(UIImage(named: "Cross.png"), for: UIControlState())
-            activePlayer = 2
-        }
-        else
-        {
-            sender.setImage(UIImage(named: "Circle.png"), for: UIControlState())
-            activePlayer = 1
+            gameState[sender.tag - 1] = activePlayer
+            
+            if activePlayer == 1
+            {
+                sender.setImage(UIImage(named: "Cross.png"), for: UIControlState())
+                activePlayer = 2
+            }
+            else
+            {
+                sender.setImage(UIImage(named: "Circle.png"), for: UIControlState())
+                activePlayer = 1
+            }
+        
         }
     }
     override func viewDidLoad() {
