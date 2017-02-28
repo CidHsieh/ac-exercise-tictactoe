b1607b152ef0504c1a1e1f3e92524c6baa694539
Cid Hsieh <cidhsieh@CidHsieh.local>
2017-02-28 21:49:32 +0800

    設定winningCombination

--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController{
     
     var activePlayer = 1 //croee
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
     @IBAction func action(_ sender: AnyObject)
     {
