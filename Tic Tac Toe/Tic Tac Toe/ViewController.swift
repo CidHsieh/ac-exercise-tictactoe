4aa4674a671890180dc88aca142891beaf1d6555
Cid Hsieh <cidhsieh@CidHsieh.local>
2017-02-28 22:08:30 +0800

    設定勝利條件，包括勝方，與遊戲狀態

--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController{
     
     var activePlayer = 1 //croee
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    var winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    var gameIsActive = true
 
     @IBAction func action(_ sender: AnyObject)
     {
@@ -31,6 +32,25 @@ class ViewController: UIViewController{
                 activePlayer = 1
             }
         
+        }
+        
+        for combination in winningCombinations
+        {
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]
+            {
+                gameIsActive = false
+                
+                if gameState[combination[0]] == 1
+                {
+                    //cross has won
+                    print("cross")
+                }
+                else
+                {
+                    //circle has won
+                    print("circle")
+                }
+            }
         }
     }
     override func viewDidLoad() {
